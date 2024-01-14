9dbea10b2cd844f070b577ec96ef4253fb7ab320
vafasaboori <saboori@gmail.com>
2024-01-13 16:12:49 -0800

    Class Exercise

--- Intro Session/ggplot_intro.R
+++ Intro Session/ggplot_intro.R
@@ -1,14 +1,15 @@
-# ggplot2 is one of the core members of the tidyverse
+# ggplot2 is one of the core members of the tidyverse ----
 install.packages("tidyverse")
 library(tidyverse)
 
-# mpg data frame found in ggplot2
+# mpg data frame found in ggplot2 ----
 library(ggplot2)
 ggplot2::mpg
 str(mpg)
-?mpg # or help(mpg)
+?mpg # or 
+help(mpg)
 
-# To plot mpg, run this code to put displ on the x-axis and hwy on the y-axis:
+# To plot mpg, we put displ on the x-axis and hwy on the y-axis ----
 ggplot(mpg, aes(x = displ, y = hwy)) +
     geom_point()
 
@@ -16,7 +17,12 @@ ggplot(mpg, aes(x = displ, y = hwy)) +
 mpg %>% ggplot(aes(x = displ, y = hwy)) + #shortcut for pipe shift + command + m 
   geom_point()
 
-# map the colors of points to the class variable to reveal the class of each car
+# Question! ----
+# A group of points fall outside of the linear trend with higher mileage
+# How can you explain these cars?
+
+# Answer ----
+## map the colors of points to the class variable to reveal the class of each car
 ggplot(mpg, aes(x = displ, y = hwy, color = class)) + 
   geom_point()
 
@@ -27,12 +33,26 @@ ggplot(mpg, aes(x = displ, y = hwy, alpha = class)) +
 # shape aesthetic controls the shape of the points
 ggplot(mpg, aes(x = displ, y = hwy, shape = class)) + 
   geom_point()
+# What happened to the SUVs? 
+# ggplot2 will only use six shapes at a time. 
+# By default, additional groups will go unplotted when you use the shape
 
 # set the aesthetic properties of your geom manually. all points blue
 ggplot(mpg, aes(x = displ, y = hwy)) + 
   geom_point(color = "blue")
+# color doesn’t convey info about a variable, only changes the appearance
 
-# split your plot into facets (subplots) each display one subset of the data
+# Class Exercise! ----
+# What’s gone wrong with this code? Why are the points not blue?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = "blue"))
+
+# Answer ----
+# "blue" within aes() is treated as a categorical variable (factor)
+# ggplot2 assigns different colors (1st red) to levels of categorical variable
+
+# split your plot into facets (subplots) ----
+# each facet display one subset of the data
 ggplot(mpg, aes(x = displ, y = hwy)) + 
   geom_point() + 
   facet_wrap(~ class, nrow = 2)
@@ -42,7 +62,13 @@ ggplot(mpg, aes(x = displ, y = hwy)) +
   geom_point() + 
   facet_grid(drv ~ cyl)
     
-# smooth geom, a smooth line fitted to the data
+# Class Exercise! ----
+# What do the empty cells in plot with facet_grid(drv ~ cyl) mean?
+
+# Answer -----
+# combinations of drv and cyl that have no observations
+
+# Geometric Objects: smooth geom, a smooth line fitted to the data ----
 ggplot(mpg, aes(x = displ, y = hwy)) + 
   geom_smooth()
 
@@ -79,7 +105,7 @@ ggplot(data = mpg, aes(x = displ, y = hwy)) +
   geom_point(aes(color = class)) + 
   geom_smooth(se = FALSE)
 
-# Scales, let's start with a simple graph
+# Geometric Objects: Scales, let's start with a simple graph ----
 ggplot(mpg, mapping = aes(x = cty, y = hwy)) + 
   geom_point()
 
@@ -90,48 +116,43 @@ ggplot(mpg, mapping = aes(x = cty, y = hwy)) +
   scale_y_reverse()
 
 # Color Sclase
+# We can use palette from the RColorBrewer package
 ggplot(mpg, mapping = aes(x = displ, y = hwy, color = class)) + 
   geom_point() +
-  scale_color_brewer()
+  scale_color_brewer() # Applies a Brewer color palette to the color scale. 
+# distinguishable color scheme for the levels of the "class" variable.
 
 # Color Palettes
 ggplot(mpg, mapping = aes(x = displ, y = hwy, color = class)) + 
   geom_point() +
-  scale_color_brewer(palette = 5)
+  scale_color_brewer(palette = "Set1") 
   
-# create a bar graph based on the class variable (categorical)
+# Geometric Objects: Bar Graphs and histograms
+# bar graph based on a categorical variable (e.g. class) ----
 ggplot(mpg, aes(x = class)) + 
   geom_bar()
 # count is a new value obtained through statistical transformation
-
-# we can do this manually
-(class_count <- dplyr::count(mpg, class))
-
-# create a bar graph based on the hwy variable (continuous)
-ggplot(mpg, aes(x = hwy)) + 
-  geom_histogram()
-
-# create a stacked bar graph based on the drv variable (categorical)
-ggplot(mpg, aes(x = class, fill = drv)) + 
-  geom_bar()
-
-# Cartesian Coordinate System
-# Limit X Axis
-ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
-  geom_point() + 
-  coord_cartesian(xlim=c(0,5))
 
 # Bar graph with flipped bars
 ggplot(mpg, aes(x=class)) +
   geom_bar() +
   coord_flip()
-  
-# Facet Grid creates subplots based on two categorical variable
+
+# create a bar graph based on a continuous variable (e.g. hwy)
+ggplot(mpg, aes(x = hwy)) + 
+  geom_histogram()
+
+# create a stacked bar graph for class var. separated by drv var. (categorical)
+ggplot(mpg, aes(x = class, fill = drv)) + 
+  geom_bar()
+
+# Cartesian Coordinate System ----
+# Limit X Axis
 ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point() + 
-  facet_grid(year ~ cyl)
-
-# Labels and Annotations
+  coord_cartesian(xlim=c(0,5))
+  
+# Labels and Annotations ----
 ggplot(mpg, aes(x = displ, y = hwy, color = class)) +
   geom_point() +
   labs(title = "Fuel Efficiency by Engine Volume",
@@ -139,4 +160,30 @@ ggplot(mpg, aes(x = displ, y = hwy, color = class)) +
        x = "Engine Volume (Liters)",
        y = "Fuel Efficiency (Miles per Gallon",
        color = "Car Type")
+
+# Facet Wrap creates subplots based on one categorical variable
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() + 
+  facet_wrap(~ cyl)
+
+# Facet Grid creates subplots based on two categorical variable
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() + 
+  facet_grid(cyl ~ fl)
+
 # ctrl + shift + 6 (Maximize Plot Window)
+
+# Class Exercise! ----
+# Create a faceted plot to compare the relationship between (displ) and (cty) 
+# across different vehicle types (class). 
+# Use facets to create subplots for each vehicle type.
+
+# Answer ----
+ggplot(mpg, aes(x = displ, y = cty)) +
+  geom_point() +
+  facet_wrap(~ class, scales = "free") +
+  labs(title = "Relationship Between Engine Displacement and City MPG by Vehicle Type",
+       x = "Engine Displacement",
+       y = "City MPG") +
+  theme_minimal()
+# Other themes include theme_bw(), theme_classic(), theme_void()
